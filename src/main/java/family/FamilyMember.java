1bbb6e45b8c118f5178b2188f287d68dd92aa92d
Evren <evren_kacar54@hotmail.com>
2023-05-22 14:58:28 +0200

    Erste klassen erstellt

---
+++ src/main/java/family/FamilyMember.java
@@ -0,0 +1,69 @@
+package family;
+
+import java.sql.Date;
+
+public class FamilyMember {
+	
+	private long id; 
+	private String firstName;
+	private String secondName;
+	private Date bDay;
+	
+
+	public FamilyMember() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public FamilyMember(long id, String firstName, String secondName, Date bDay) {
+		
+	}
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	//@return the secondName
+	public String getSecondName() {
+		return secondName;
+	}
+
+	// @param secondName the secondName to set
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	//@return the bDay
+	public Date getbDay() {
+		return bDay;
+	}
+
+	//@param bDay the bDay to set
+	public void setbDay(Date bDay) {
+		this.bDay = bDay;
+	}
+
+}
