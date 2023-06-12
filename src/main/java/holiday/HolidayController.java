46d02698af5daada086ecd23b59be21c04e594f0
Simon Herrlinger <Simon.Herrlinger16@gmail.com>
2023-06-12 15:34:42 +0200

    Äderung HolidayController

--- src/main/java/holiday/HolidayController.java
+++ src/main/java/holiday/HolidayController.java
@@ -1,9 +1,21 @@
 package holiday;
 
-public class HolidayController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-	public HolidayController() {
-		// TODO Auto-generated constructor stub
-	}
+//import hs.aalen.Holidaywish.HolidayWish;
+@RestController
+public class HolidayController {
+	@Autowired
+	HolidayService holidayservice;
+	
+	/*@RequestMapping("/holiday")
+	public List<Holiday> getHolidayList() {
+		return holidayservice.getHolidayList();
+	}*/
 
 }
