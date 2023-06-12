abc7143e39c0b7653512c6e4b9a3d6893c5d205b
Simon Herrlinger <Simon.Herrlinger16@gmail.com>
2023-06-12 16:03:03 +0200

    Automatische Generierung von Attribut ID in FamilyMember

--- src/main/java/family/FamilyMember.java
+++ src/main/java/family/FamilyMember.java
@@ -1,13 +1,21 @@
 package family;
 
 import java.sql.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
+@Entity
 public class FamilyMember {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+		
 	private long id; 
 	private String firstName;
 	private String secondName; 
 	private Date bDay;
+	
 	
 
 	public FamilyMember() {
