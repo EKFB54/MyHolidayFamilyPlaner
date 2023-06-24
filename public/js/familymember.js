961ac4210f438bcfa3e567d318016ca8e6ceecc2
Evren <evren_kacar54@hotmail.com>
2023-06-24 14:08:46 +0200

    Js zur erstellung der familienmitglieder

--- public/js/familymember.js
+++ public/js/familymember.js
@@ -1,52 +1,35 @@
 $(document).ready(function() {
-	loadDataTable();
-	//process the form newVideo
-	$("#newF").submit(function(event) {
-		postFamily(event);
-	});
-	//Load Datatable
-	$('#loadtable').click(function() {
-		loadDataTable();
-	});
-});
-function postVideo(event) {
-	// get the form data
-	var formData = {
-		'id': $('input[name=id]').val(),
-		'firstName': $('textarea[name=firstName]').val(),
-		'secondName': $('input[name = secondName]').val(),
-		'bDay': $('input[name=bDay]').val()
-	};
-	// process the form
-	$.ajax({
-		type: 'POST', // define the type of HTTP verb we want to use(POST for our form)
-		contentType: 'application/json',
-		url: 'http://localhost:8080/familymember/{id}', // url where we want to POST
-		data: JSON.stringify(formData), // data we want to POST
-		success: function(data, textStatus, jQxhr) {
-			loadDataTable();
-		},
-		error: function(jqXhr, textStatus, errorThrown) {
-			console.log(errorThrown);
-		}
-	});
-	// stop the form from submitting the normal way and refreshing the
-	page
-	event.preventDefault();
-}
+  $('#createFamily').submit(function(event) {
+    event.preventDefault();
 
-function loadDataTable() {
-	var table = $('#Familytable').DataTable({
-		destroy: true,
-		"ajax": {
-			"url": "http://localhost:8080/familymember", //URL
-			"dataSrc": "" // Cause of flat JsonObjects
-		},
-		"columns": [
-			{ "data": "id" },
-			{ "data": "firstName" },
-			{ "data": "secondName" },
-			{ "data": "bDay" }
-		]
-	});
-}
+    var firstName = $('#firstName').val();
+    var secondName = $('#secondName').val();
+    var bDay = $('#bDay').val();
+
+    var familyMember = {
+      firstName: firstName,
+      secondName: secondName,
+      bDay: bDay
+    };
+
+    // AJAX-Anfrage an den Server senden
+    $.ajax({
+      url: 'http://localhost:8080/familymember',
+      type: 'POST',
+      data: JSON.stringify(familyMember),
+      contentType: 'application/json',
+      success: function(response) {
+        // Erfolgreiche Antwort vom Server erhalten
+        console.log('Familienmitglied erfolgreich hinzugefügt:', response);
+
+        // Hier kannst du den Code einfügen, um eine Bestätigungsmeldung anzuzeigen oder andere Aktionen auszuführen
+      },
+      error: function(error) {
+        // Fehler bei der AJAX-Anfrage
+        console.error('Fehler beim Hinzufügen des Familienmitglieds:', error);
+      }
+    });
+
+    $('#createFamily')[0].reset();
+  });
+});
