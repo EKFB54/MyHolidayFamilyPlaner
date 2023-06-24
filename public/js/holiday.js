0e54fa33fbefdd991174bf5825ed8ed1c1d946df
Simon Herrlinger <Simon.Herrlinger16@gmail.com>
2023-06-24 22:12:51 +0200

    loadtable Holiday, CSS muss noch überarbeitet werden

--- public/js/holiday.js
+++ public/js/holiday.js
@@ -35,4 +35,49 @@ $(document).ready(function() {
 
     $('#createHoliday')[0].reset();
   });
+  // Add event listener for each delete button
+  $('.delete-button').each(function() {
+    $(this).click(function() {
+      var memberId = $(this).attr('id');
+
+      $.ajax({
+        url: 'http://localhost:8080/familymember/' + memberId,
+        type: 'DELETE',
+        success: function(jqXhr, textStatus, errorThrown) {
+          console.log('Successfully deleted family member');
+        },
+        error: function(jqXhr, textStatus, errorThrown) {
+          console.log(errorThrown);
+        }
+      });
+    });
+  });
+
+  function loadDataTable() {
+    var table = $('#holdaySelectionList').DataTable({
+      destroy: true,
+      ajax: {
+        url: 'http://localhost:8080/holiday',
+        type: 'GET',
+        dataType: 'json',
+        dataSrc: ''
+      },
+      columns: [
+        { data: 'ort' },
+        { data: 'land' },
+        { data: 'preis' },
+        { data: 'ab'},
+        { data: 'bis'},
+        {
+          data: null,
+          render: function (data, type, row) {
+            return '<button class="delete-button" id="' + row.id + '"><img src="images/delete.png" alt="Delete"></button>';
+          }
+        }
+      ]
+    });
+  }
+
+  loadDataTable();
 });
+
