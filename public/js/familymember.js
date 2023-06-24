9ad29d7c564cd357c50574ef2594d13cda1d564c
Evren <evren_kacar54@hotmail.com>
2023-06-24 14:18:41 +0200

    erstellung von holiday.js

--- public/js/familymember.js
+++ public/js/familymember.js
@@ -12,20 +12,20 @@ $(document).ready(function() {
       bDay: bDay
     };
 
-    // AJAX-Anfrage an den Server senden
+    
     $.ajax({
       url: 'http://localhost:8080/familymember',
       type: 'POST',
       data: JSON.stringify(familyMember),
       contentType: 'application/json',
       success: function(response) {
-        // Erfolgreiche Antwort vom Server erhalten
+        
         console.log('Familienmitglied erfolgreich hinzugefügt:', response);
 
-        // Hier kannst du den Code einfügen, um eine Bestätigungsmeldung anzuzeigen oder andere Aktionen auszuführen
+        
       },
       error: function(error) {
-        // Fehler bei der AJAX-Anfrage
+        
         console.error('Fehler beim Hinzufügen des Familienmitglieds:', error);
       }
     });
